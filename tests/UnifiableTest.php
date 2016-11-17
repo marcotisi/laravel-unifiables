13cf9172832a7d47089e7de9c13559639b27ffc5
Marco Tisi <marco.tisi@gmail.com>
2016-11-17 22:03:44 +0100

    Add test

--- tests/UnifiableTest.php
+++ tests/UnifiableTest.php
@@ -2,6 +2,7 @@
 
 namespace MarcoTisi\Unifiables\Test;
 
+use Illuminate\Database\Eloquent\Collection;
 use MarcoTisi\Unifiables\Test\Models\EventTest;
 use MarcoTisi\Unifiables\Test\Models\NewsTest;
 use MarcoTisi\Unifiables\Unifiable;
@@ -37,5 +38,22 @@ class UnifiableTest extends TestCase
 
         $this->assertArrayHasKey('MarcoTisi\Unifiables\Test\Models\NewsTest', $unifiable->getUnifiables());
         $this->assertArrayHasKey('MarcoTisi\Unifiables\Test\Models\EventTest', $unifiable->getUnifiables());
+    }
+
+    public function testResults()
+    {
+        $unifiable = new Unifiable();
+
+        $unifiable->addUnifiable(NewsTest::class, [
+            'date' => 'published_at',
+        ]);
+
+        $unifiable->addUnifiable(EventTest::class, [
+            'title'    => 'name',
+            'subtitle' => 'venue',
+            'date'     => 'held_at',
+        ]);
+
+        $this->assertInstanceOf(Collection::class, $unifiable->get());
     }
 }
